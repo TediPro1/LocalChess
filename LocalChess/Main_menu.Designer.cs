4fc60118a5568a956a06794599508d73e4fcd516
Tedi-dev <tp20931863@edu.mon.bg>
2026-05-26 12:44:32 +0300

    polished ui

--- LocalChess/Main_menu.Designer.cs
+++ LocalChess/Main_menu.Designer.cs
@@ -38,6 +38,7 @@
             join_pass_hide_button = new Button();
             label1 = new Label();
             join_game_pass = new TextBox();
+            button7 = new Button();
             button4 = new Button();
             button3 = new Button();
             listBox1 = new ListBox();
@@ -52,7 +53,6 @@
             game_history_page = new TabPage();
             button8 = new Button();
             flowLayoutPanel1 = new FlowLayoutPanel();
-            button7 = new Button();
             menu.SuspendLayout();
             main_page.SuspendLayout();
             join_game_page.SuspendLayout();
@@ -66,11 +66,10 @@
             menu.Controls.Add(join_game_page);
             menu.Controls.Add(new_game_page);
             menu.Controls.Add(game_history_page);
-            menu.Location = new Point(-4, -22);
-            menu.Margin = new Padding(3, 2, 3, 2);
+            menu.Location = new Point(-5, -29);
             menu.Name = "menu";
             menu.SelectedIndex = 0;
-            menu.Size = new Size(710, 364);
+            menu.Size = new Size(811, 485);
             menu.TabIndex = 0;
             // 
             // main_page
@@ -80,11 +79,10 @@
             main_page.Controls.Add(isOnlineCheckBox);
             main_page.Controls.Add(button2);
             main_page.Controls.Add(button1);
-            main_page.Location = new Point(4, 24);
-            main_page.Margin = new Padding(3, 2, 3, 2);
+            main_page.Location = new Point(4, 29);
             main_page.Name = "main_page";
-            main_page.Padding = new Padding(3, 2, 3, 2);
-            main_page.Size = new Size(702, 336);
+            main_page.Padding = new Padding(3);
+            main_page.Size = new Size(803, 452);
             main_page.TabIndex = 0;
             main_page.Text = "tabPage1";
             main_page.UseVisualStyleBackColor = true;
@@ -93,19 +91,24 @@
             // 
             isOnlineCheckBox.AutoSize = true;
             isOnlineCheckBox.ForeColor = SystemColors.Control;
-            isOnlineCheckBox.Location = new Point(321, 244);
+            isOnlineCheckBox.Location = new Point(367, 325);
+            isOnlineCheckBox.Margin = new Padding(3, 4, 3, 4);
             isOnlineCheckBox.Name = "isOnlineCheckBox";
-            isOnlineCheckBox.Size = new Size(61, 19);
+            isOnlineCheckBox.Size = new Size(74, 24);
             isOnlineCheckBox.TabIndex = 7;
             isOnlineCheckBox.Text = "Online";
             isOnlineCheckBox.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            button2.Location = new Point(395, 235);
-            button2.Margin = new Padding(3, 2, 3, 2);
+            button2.FlatAppearance.BorderColor = Color.White;
+            button2.FlatAppearance.BorderSize = 5;
+            button2.FlatAppearance.MouseOverBackColor = Color.DarkGray;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(451, 313);
             button2.Name = "button2";
-            button2.Size = new Size(115, 34);
+            button2.Size = new Size(131, 45);
             button2.TabIndex = 0;
             button2.Text = "Join Game";
             button2.UseVisualStyleBackColor = true;
@@ -113,10 +116,14 @@
             // 
             // button1
             // 
-            button1.Location = new Point(192, 235);
-            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.FlatAppearance.BorderColor = Color.White;
+            button1.FlatAppearance.BorderSize = 5;
+            button1.FlatAppearance.MouseOverBackColor = Color.DarkGray;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(219, 313);
             button1.Name = "button1";
-            button1.Size = new Size(115, 34);
+            button1.Size = new Size(131, 45);
             button1.TabIndex = 0;
             button1.Text = "New Game";
             button1.UseVisualStyleBackColor = true;
@@ -133,50 +140,73 @@
             join_game_page.Controls.Add(button4);
             join_game_page.Controls.Add(button3);
             join_game_page.Controls.Add(listBox1);
-            join_game_page.Location = new Point(4, 24);
-            join_game_page.Margin = new Padding(3, 2, 3, 2);
+            join_game_page.Location = new Point(4, 29);
             join_game_page.Name = "join_game_page";
-            join_game_page.Padding = new Padding(3, 2, 3, 2);
-            join_game_page.Size = new Size(702, 336);
+            join_game_page.Padding = new Padding(3);
+            join_game_page.Size = new Size(803, 452);
             join_game_page.TabIndex = 1;
             join_game_page.Text = "tabPage2";
             join_game_page.UseVisualStyleBackColor = true;
             // 
             // join_pass_hide_button
             // 
+            join_pass_hide_button.BackColor = Color.White;
             join_pass_hide_button.BackgroundImageLayout = ImageLayout.Stretch;
-            join_pass_hide_button.Location = new Point(556, 157);
+            join_pass_hide_button.FlatAppearance.BorderColor = Color.White;
+            join_pass_hide_button.FlatAppearance.MouseOverBackColor = Color.DarkGray;
+            join_pass_hide_button.FlatStyle = FlatStyle.Flat;
+            join_pass_hide_button.ForeColor = Color.White;
+            join_pass_hide_button.Location = new Point(633, 209);
+            join_pass_hide_button.Margin = new Padding(3, 4, 3, 4);
             join_pass_hide_button.Name = "join_pass_hide_button";
-            join_pass_hide_button.Size = new Size(25, 25);
+            join_pass_hide_button.Size = new Size(27, 27);
             join_pass_hide_button.TabIndex = 8;
-            join_pass_hide_button.UseVisualStyleBackColor = true;
+            join_pass_hide_button.UseVisualStyleBackColor = false;
             join_pass_hide_button.Click += button7_Click_1;
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.ForeColor = Color.Transparent;
-            label1.Location = new Point(346, 140);
+            label1.Location = new Point(395, 187);
             label1.Name = "label1";
-            label1.Size = new Size(57, 15);
+            label1.Size = new Size(70, 20);
             label1.TabIndex = 3;
             label1.Text = "Password";
             // 
             // join_game_pass
             // 
-            join_game_pass.Location = new Point(346, 157);
-            join_game_pass.Margin = new Padding(3, 2, 3, 2);
+            join_game_pass.Location = new Point(395, 209);
             join_game_pass.Name = "join_game_pass";
-            join_game_pass.Size = new Size(204, 23);
+            join_game_pass.Size = new Size(233, 27);
             join_game_pass.TabIndex = 2;
             join_game_pass.UseSystemPasswordChar = true;
             // 
+            // button7
+            // 
+            button7.FlatAppearance.BorderColor = Color.White;
+            button7.FlatAppearance.BorderSize = 5;
+            button7.FlatAppearance.MouseOverBackColor = Color.DarkGray;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.ForeColor = Color.White;
+            button7.Location = new Point(679, 5);
+            button7.Name = "button7";
+            button7.Size = new Size(117, 41);
+            button7.TabIndex = 1;
+            button7.Text = "Game History";
+            button7.UseVisualStyleBackColor = true;
+            button7.Click += button7_Click_2;
+            // 
             // button4
             // 
-            button4.Location = new Point(596, 306);
-            button4.Margin = new Padding(3, 2, 3, 2);
+            button4.FlatAppearance.BorderColor = Color.White;
+            button4.FlatAppearance.BorderSize = 5;
+            button4.FlatAppearance.MouseOverBackColor = Color.DarkGray;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.ForeColor = Color.White;
+            button4.Location = new Point(681, 408);
             button4.Name = "button4";
-            button4.Size = new Size(102, 31);
+            button4.Size = new Size(117, 41);
             button4.TabIndex = 1;
             button4.Text = "Back";
             button4.UseVisualStyleBackColor = true;
@@ -184,10 +214,14 @@
             // 
             // button3
             // 
-            button3.Location = new Point(346, 306);
-            button3.Margin = new Padding(3, 2, 3, 2);
+            button3.FlatAppearance.BorderColor = Color.White;
+            button3.FlatAppearance.BorderSize = 5;
+            button3.FlatAppearance.MouseOverBackColor = Color.DarkGray;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.ForeColor = Color.White;
+            button3.Location = new Point(395, 408);
             button3.Name = "button3";
-            button3.Size = new Size(102, 31);
+            button3.Size = new Size(117, 41);
             button3.TabIndex = 1;
             button3.Text = "Join";
             button3.UseVisualStyleBackColor = true;
@@ -196,11 +230,9 @@
             // listBox1
             // 
             listBox1.FormattingEnabled = true;
-            listBox1.ItemHeight = 15;
             listBox1.Location = new Point(0, 0);
-            listBox1.Margin = new Padding(3, 2, 3, 2);
             listBox1.Name = "listBox1";
-            listBox1.Size = new Size(341, 349);
+            listBox1.Size = new Size(389, 464);
             listBox1.TabIndex = 0;
             // 
             // new_game_page
@@ -214,68 +246,78 @@
             new_game_page.Controls.Add(new_game_name);
             new_game_page.Controls.Add(button5);
             new_game_page.Controls.Add(button6);
-            new_game_page.Location = new Point(4, 24);
-            new_game_page.Margin = new Padding(3, 2, 3, 2);
+            new_game_page.Location = new Point(4, 29);
             new_game_page.Name = "new_game_page";
-            new_game_page.Padding = new Padding(3, 2, 3, 2);
-            new_game_page.Size = new Size(702, 336);
+            new_game_page.Padding = new Padding(3);
+            new_game_page.Size = new Size(803, 452);
             new_game_page.TabIndex = 2;
             new_game_page.Text = "tabPage3";
             new_game_page.UseVisualStyleBackColor = true;
             // 
             // hidePassButton
             // 
+            hidePassButton.BackColor = Color.White;
             hidePassButton.BackgroundImageLayout = ImageLayout.Stretch;
-            hidePassButton.Location = new Point(454, 204);
+            hidePassButton.FlatAppearance.BorderColor = Color.White;
+            hidePassButton.FlatAppearance.MouseOverBackColor = Color.Gray;
+            hidePassButton.FlatStyle = FlatStyle.Flat;
+            hidePassButton.Location = new Point(460, 272);
+            hidePassButton.Margin = new Padding(3, 4, 3, 4);
             hidePassButton.Name = "hidePassButton";
-            hidePassButton.Size = new Size(25, 25);
+            hidePassButton.Size = new Size(27, 27);
             hidePassButton.TabIndex = 7;
-            hidePassButton.UseVisualStyleBackColor = true;
+            hidePassButton.UseVisualStyleBackColor = false;
             hidePassButton.Click += button7_Click;
             // 
             // label3
             // 
             label3.AutoSize = true;
+            label3.BorderStyle = BorderStyle.FixedSingle;
+            label3.FlatStyle = FlatStyle.Flat;
             label3.ForeColor = Color.White;
-            label3.Location = new Point(326, 180);
+            label3.Location = new Point(315, 247);
             label3.Name = "label3";
-            label3.Size = new Size(112, 15);
+            label3.Size = new Size(142, 22);
             label3.TabIndex = 5;
             label3.Text = "Password (optional)";
             // 
             // label2
             // 
             label2.AutoSize = true;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.FlatStyle = FlatStyle.Flat;
             label2.ForeColor = Color.White;
-            label2.Location = new Point(326, 112);
+            label2.Location = new Point(315, 165);
             label2.Name = "label2";
-            label2.Size = new Size(39, 15);
+            label2.Size = new Size(51, 22);
             label2.TabIndex = 5;
             label2.Text = "Name";
             // 
             // new_game_pass
             // 
-            new_game_pass.Location = new Point(326, 204);
-            new_game_pass.Margin = new Padding(3, 2, 3, 2);
+            new_game_pass.Location = new Point(315, 272);
             new_game_pass.Name = "new_game_pass";
-            new_game_pass.Size = new Size(110, 23);
+            new_game_pass.Size = new Size(142, 27);
             new_game_pass.TabIndex = 4;
             new_game_pass.UseSystemPasswordChar = true;
             // 
             // new_game_name
             // 
-            new_game_name.Location = new Point(326, 130);
-            new_game_name.Margin = new Padding(3, 2, 3, 2);
+            new_game_name.Location = new Point(315, 189);
             new_game_name.Name = "new_game_name";
-            new_game_name.Size = new Size(110, 23);
+            new_game_name.Size = new Size(142, 27);
             new_game_name.TabIndex = 4;
             // 
             // button5
             // 
-            button5.Location = new Point(596, 305);
-            button5.Margin = new Padding(3, 2, 3, 2);
+            button5.FlatAppearance.BorderColor = Color.White;
+            button5.FlatAppearance.BorderSize = 5;
+            button5.FlatAppearance.MouseOverBackColor = Color.DarkGray;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.ForeColor = Color.White;
+            button5.Location = new Point(680, 405);
             button5.Name = "button5";
-            button5.Size = new Size(102, 31);
+            button5.Size = new Size(117, 41);
             button5.TabIndex = 2;
             button5.Text = "Back";
             button5.UseVisualStyleBackColor = true;
@@ -283,10 +325,14 @@
             // 
             // button6
             // 
-            button6.Location = new Point(3, 304);
-            button6.Margin = new Padding(3, 2, 3, 2);
+            button6.FlatAppearance.BorderColor = Color.White;
+            button6.FlatAppearance.BorderSize = 5;
+            button6.FlatAppearance.MouseOverBackColor = Color.DarkGray;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.ForeColor = Color.White;
+            button6.Location = new Point(6, 405);
             button6.Name = "button6";
-            button6.Size = new Size(102, 31);
+            button6.Size = new Size(117, 41);
             button6.TabIndex = 3;
             button6.Text = "Create";
             button6.UseVisualStyleBackColor = true;
@@ -296,20 +342,25 @@
             // 
             game_history_page.Controls.Add(button8);
             game_history_page.Controls.Add(flowLayoutPanel1);
-            game_history_page.Location = new Point(4, 24);
+            game_history_page.Location = new Point(4, 29);
+            game_history_page.Margin = new Padding(3, 4, 3, 4);
             game_history_page.Name = "game_history_page";
-            game_history_page.Padding = new Padding(3);
-            game_history_page.Size = new Size(702, 336);
+            game_history_page.Padding = new Padding(3, 4, 3, 4);
+            game_history_page.Size = new Size(803, 452);
             game_history_page.TabIndex = 3;
             game_history_page.Text = "History";
             game_history_page.UseVisualStyleBackColor = true;
             // 
             // button8
             // 
-            button8.Location = new Point(594, 300);
-            button8.Margin = new Padding(3, 2, 3, 2);
+            button8.FlatAppearance.BorderColor = Color.Black;
+            button8.FlatAppearance.BorderSize = 3;
+            button8.FlatAppearance.MouseOverBackColor = Color.Gainsboro;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.ForeColor = Color.Black;
+            button8.Location = new Point(680, 407);
             button8.Name = "button8";
-            button8.Size = new Size(102, 31);
+            button8.Size = new Size(117, 41);
             button8.TabIndex = 2;
             button8.Text = "Back";
             button8.UseVisualStyleBackColor = true;
@@ -319,32 +370,22 @@
             // 
             flowLayoutPanel1.AutoScroll = true;
             flowLayoutPanel1.Dock = DockStyle.Fill;
-            flowLayoutPanel1.Location = new Point(3, 3);
+            flowLayoutPanel1.Location = new Point(3, 4);
+            flowLayoutPanel1.Margin = new Padding(3, 4, 3, 4);
             flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(696, 330);
+            flowLayoutPanel1.Size = new Size(797, 444);
             flowLayoutPanel1.TabIndex = 0;
-            // 
-            // button7
-            // 
-            button7.Location = new Point(594, 4);
-            button7.Margin = new Padding(3, 2, 3, 2);
-            button7.Name = "button7";
-            button7.Size = new Size(102, 31);
-            button7.TabIndex = 1;
-            button7.Text = "Game History";
-            button7.UseVisualStyleBackColor = true;
-            button7.Click += button7_Click_2;
             // 
             // Main_menu
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackgroundImageLayout = ImageLayout.Stretch;
-            ClientSize = new Size(700, 338);
+            ClientSize = new Size(800, 451);
             Controls.Add(menu);
             FormBorderStyle = FormBorderStyle.FixedSingle;
             Icon = (Icon)resources.GetObject("$this.Icon");
-            Margin = new Padding(3, 2, 3, 2);
+            MaximizeBox = false;
             Name = "Main_menu";
             Text = "Main menu";
             FormClosed += Main_menu_FormClosed;
--- LocalChess/ShowChessGame.Designer.cs
+++ LocalChess/ShowChessGame.Designer.cs
@@ -40,19 +40,27 @@
             boardPanel.ColumnCount = 2;
             boardPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             boardPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
-            boardPanel.Location = new Point(3, 76);
+            boardPanel.Location = new Point(3, 101);
+            boardPanel.Margin = new Padding(3, 4, 3, 4);
             boardPanel.Name = "boardPanel";
             boardPanel.RowCount = 2;
             boardPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             boardPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
-            boardPanel.Size = new Size(200, 200);
+            boardPanel.Size = new Size(229, 267);
             boardPanel.TabIndex = 0;
             // 
             // button1
             // 
-            button1.Location = new Point(128, 282);
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderColor = Color.Black;
+            button1.FlatAppearance.BorderSize = 2;
+            button1.FlatAppearance.MouseOverBackColor = Color.FromArgb(255, 255, 192);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.ForeColor = Color.Black;
+            button1.Location = new Point(144, 375);
+            button1.Margin = new Padding(3, 4, 3, 4);
             button1.Name = "button1";
-            button1.Size = new Size(75, 23);
+            button1.Size = new Size(86, 31);
             button1.TabIndex = 1;
             button1.Text = "Open";
             button1.UseVisualStyleBackColor = true;
@@ -61,42 +69,47 @@
             // winner_label
             // 
             winner_label.AutoSize = true;
-            winner_label.Location = new Point(3, 28);
+            winner_label.BorderStyle = BorderStyle.FixedSingle;
+            winner_label.Location = new Point(3, 37);
             winner_label.Name = "winner_label";
-            winner_label.Size = new Size(45, 15);
+            winner_label.Size = new Size(58, 22);
             winner_label.TabIndex = 2;
             winner_label.Text = "Winner";
             // 
             // win_condition_label
             // 
             win_condition_label.AutoSize = true;
-            win_condition_label.Location = new Point(3, 48);
+            win_condition_label.BorderStyle = BorderStyle.FixedSingle;
+            win_condition_label.Location = new Point(3, 64);
             win_condition_label.Name = "win_condition_label";
-            win_condition_label.Size = new Size(84, 15);
+            win_condition_label.Size = new Size(106, 22);
             win_condition_label.TabIndex = 2;
             win_condition_label.Text = "Win Condition";
             // 
             // lobby_name_label
             // 
             lobby_name_label.AutoSize = true;
-            lobby_name_label.Location = new Point(3, 8);
+            lobby_name_label.BorderStyle = BorderStyle.FixedSingle;
+            lobby_name_label.Location = new Point(3, 11);
             lobby_name_label.Name = "lobby_name_label";
-            lobby_name_label.Size = new Size(39, 15);
+            lobby_name_label.Size = new Size(51, 22);
             lobby_name_label.TabIndex = 2;
             lobby_name_label.Text = "Name";
             // 
             // ShowChessGame
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.PapayaWhip;
+            BorderStyle = BorderStyle.FixedSingle;
             Controls.Add(win_condition_label);
             Controls.Add(lobby_name_label);
             Controls.Add(winner_label);
             Controls.Add(button1);
             Controls.Add(boardPanel);
+            Margin = new Padding(3, 4, 3, 4);
             Name = "ShowChessGame";
-            Size = new Size(206, 308);
+            Size = new Size(233, 409);
             Load += ShowChessGame_Load;
             ResumeLayout(false);
             PerformLayout();
